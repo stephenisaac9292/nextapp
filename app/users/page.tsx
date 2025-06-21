24e41b9ee61277e8d9437b82549e54e9e40b0cfa
Stephen Isaac <stephenisaac9292@gmail.com>
2025-06-21 01:05:15 +0100

    save1

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
+interface User {
+    id: number;
+    name: string;
+}
 
 
-const usersPage = () => {
+const usersPage = async () => {
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+
+    const users: User[]= await res.json()
+    
   return (
-    <main>
-        <div>usersPage</div>
-      
-    </main>
+    <>
+        
+        <h1>Users</h1>
+        <ul>
+        {users.map(user => 
+            <li key={user.id}>
+                <h2>{user.id}</h2> 
+                <h2>{user.name}</h2>
+            </li>
+        )}
+        </ul>
+
+    </>
   )
 }
 
